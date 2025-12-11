6f56428e5aa58ca6de0ffbc74db5799d3eaad095
zy-14 <>
2025-12-11 12:20:05 +0800

    新建项目

---
+++ WhatsWeb03/WhatsWeb03/WhatsWeb03App.swift
@@ -0,0 +1,17 @@
+//
+//  WhatsWeb03App.swift
+//  WhatsWeb03
+//
+//  Created by zy-14 on 2025/12/11.
+//
+
+import SwiftUI
+
+@main
+struct WhatsWeb03App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
